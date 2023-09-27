84b5ddbce1716234a42c0b6463a4cc748e92b0e2
Sanjith17 <petapallisanjith@gmail.com>
2023-09-26 21:28:41 -0400

    sync

--- mern-setup/client/src/App.jsx
+++ mern-setup/client/src/App.jsx
@@ -1,10 +1,22 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
+import { getTest } from "./functions/test";
+
 function App() {
+  const [data,setData] = useState("Hello World!");
+
+  useEffect(() => {
+    getTest()
+      .then((res) => {
+          setData(res.message);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="App">
-      <h1>Header</h1>
+      <h1>{data}</h1>
     </div>
   );
 }
