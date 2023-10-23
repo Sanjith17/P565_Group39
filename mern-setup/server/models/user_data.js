ddff02e4c808cf4a5df208cd1ed4657173a6d6e9
Sanjith17 <petapallisanjith@gmail.com>
2023-10-22 21:56:34 -0400

    work in progress forgotpassword

--- mern-setup/server/models/user_data.js
+++ mern-setup/server/models/user_data.js
@@ -13,15 +13,3 @@ const User = new mongoose.Schema(
 {collection: 'user-data'})
 
 module.exports = mongoose.model('UserData', User);
-
-const gauth = new mongoose.Schema(
-  {
-    name: { type: String, required: true},
-
-role:{type: String, required:true},
-email:{type: String, required: true},
-loginid:{type: String, required: true}
-},
-{collection: 'auth-data'})
-
-module.exports = mongoose.model('authData', gauth);
