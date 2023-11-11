1983b42398c646053211f4a843da9457bddb7de4
Prateek Giridhar <prateek.giridhar2000@gmail.com>
2023-11-11 12:02:31 -0500

    Working Copy

--- mern-setup/client/src/Login/Login.jsx
+++ mern-setup/client/src/Login/Login.jsx
@@ -1,30 +1,47 @@
-import React, { useState } from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
-import './LoginCss.css';
-import { Link, useNavigate } from 'react-router-dom';
-import app from '../Firebase/firebase';
-import GoogleIcon from './google-icon.png'
+import React, { useState } from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import "./LoginCss.css";
+import { Link, useNavigate } from "react-router-dom";
+import app from "../Firebase/firebase";
+import GoogleIcon from "./google-icon.png";
 
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
-  const [loginMessage, setLoginMessage] = useState('');
+  const [loginMessage, setLoginMessage] = useState("");
   const navigate = useNavigate(); // Move useNavigate to the component level
 
   const handleInputChange = (event) => {
     const { name, value, type, checked } = event.target;
-    const inputValue = type === 'checkbox' ? checked : value;
+    const inputValue = type === "checkbox" ? checked : value;
 
-    if (name === 'username') {
+    if (name === "username") {
       setUsername(inputValue);
-    } else if (name === 'password') {
+    } else if (name === "password") {
       setPassword(inputValue);
-    } else if (name === 'rememberMe') {
+    } else if (name === "rememberMe") {
       setRememberMe(inputValue);
     }
+  };
+
+  const handleMFA = async () => {
+
+    const mfaAuthCode = prompt('Please enter the Google authentication Code')
+
+    const mfaStatus = await fetch(process.env.REACT_APP_BACKEND_URL + "/api/duo-auth", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ mfaAuthCode })
+    })
+
+    const result = await mfaStatus.json()
+
+
   };
 
   const signInWithGoogle = async () => {
@@ -37,27 +54,28 @@ function Login() {
 
       const idToken = await result.user.getIdToken();
 
-      console.log(process.env.REACT_APP_BACKEND_URL)
-      const response = await fetch(process.env.REACT_APP_BACKEND_URL+'/auth', {
-
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ idToken }),
-      });
+      console.log(process.env.REACT_APP_BACKEND_URL);
+      const response = await fetch(
+        process.env.REACT_APP_BACKEND_URL + "/auth",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ idToken }),
+        }
+      );
 
       const data = await response.json();
 
       console.log(data);
 
       if (data.success) {
-        alert('You are being redirected to the user Dashboard');
-
-        navigate('/dashboard');
+        alert("You are being redirected to the user Dashboard");
+        navigate("/dashboard");
       } else {
-        alert('You are being redirected to the Landing page');
-        navigate('/');
+        alert("You are being redirected to the Landing page");
+        navigate("/");
       }
     } catch (error) {
       console.log(error.message);
@@ -72,39 +90,40 @@ function Login() {
     };
 
     try {
-
-      console.log(process.env.REACT_APP_BACKEND_URL)
-      const response = await fetch(process.env.REACT_APP_BACKEND_URL+'/login', {
-
-
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
+      console.log(process.env.REACT_APP_BACKEND_URL);
+      const response = await fetch(
+        process.env.REACT_APP_BACKEND_URL + "/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
       const responseJSON = await response.json();
 
       if (response.ok) {
-        console.log('Login successful!');
+        console.log("Login successful!");
         console.log(responseJSON.message);
         setLoginMessage(responseJSON.message);
 
+        Cookies.set("loginToken", responseJSON.jwt_token);
 
         localStorage.setItem('loginToken', responseJSON.jwt_token);
 
         navigate('/customer');
       } else {
-        console.log('Login failed');
+        console.log("Login failed");
         setLoginMessage(responseJSON.message);
       }
     } catch (error) {
-      console.error('An error occurred:', error);
+      console.error("An error occurred:", error);
     }
   };
 
-  return(
+  return (
     <div>
       <nav>
         <ul>
@@ -126,8 +145,8 @@ function Login() {
               value={username}
               onChange={handleInputChange}
               style={{
-                margin: '0 auto',
-                maxWidth: '350px',
+                margin: "0 auto",
+                maxWidth: "350px",
               }}
             />
           </div>
@@ -140,8 +159,8 @@ function Login() {
               value={password}
               onChange={handleInputChange}
               style={{
-                margin: '0 auto',
-                maxWidth: '350px',
+                margin: "0 auto",
+                maxWidth: "350px",
               }}
             />
           </div>
@@ -161,9 +180,9 @@ function Login() {
               type="button"
               onClick={handleLogin}
               style={{
-                backgroundColor: '#f00',
-                margin: '0 auto',
-                maxWidth: '350px',
+                backgroundColor: "#f00",
+                margin: "0 auto",
+                maxWidth: "350px",
               }}
             >
               Login
@@ -171,17 +190,17 @@ function Login() {
             <button
               type="button"
               style={{
-                backgroundColor: '#fff',
-                color: '#000',
-                border: '1px solid #ccc',
-                padding: '10px 20px',
-                marginTop: '10px',
-                cursor: 'pointer',
-                display: 'inline-block',
-                borderRadius: '5px',
-                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                maxWidth: '350px',
-                alignSelf: 'center',
+                backgroundColor: "#fff",
+                color: "#000",
+                border: "1px solid #ccc",
+                padding: "10px 20px",
+                marginTop: "10px",
+                cursor: "pointer",
+                display: "inline-block",
+                borderRadius: "5px",
+                boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+                maxWidth: "350px",
+                alignSelf: "center",
               }}
               onClick={signInWithGoogle}
             >
@@ -190,7 +209,7 @@ function Login() {
                 alt="Google Icon"
                 width="20"
                 height="20"
-                style={{ verticalAlign: 'middle', marginRight: '10px' }}
+                style={{ verticalAlign: "middle", marginRight: "10px" }}
               />
               Sign in with Google
             </button>
