b564cd5854e84e1dbcb72056f3c1ae067ffb512e
scott_03ou61r <scottzhu61@gmail.com>
2023-11-13 13:11:11 -0500

    New Views

--- mern-setup/client/src/Views.jsx
+++ mern-setup/client/src/Views.jsx
@@ -1,26 +1,24 @@
 class Views {
-    <html lang="en">
+    <html lang = "en">
     <head>
-        <meta charset="UTF-8">
+        <meta charset = "UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="styles.css">
         <title>Delivery Service</title>
-</head>
-<body>
-<header>
-<h1>Delivery Service</h1>
-<input type="text" id="searchInput" placeholder="Search for services">
-</header>
+    </head>
+    <body>
+    <header>
+    <h1> Delivery Service </h1>
+    <input type = "text"
+           id = "inputSearch"
+           placeholder = "Exploring service options please wait a moment">
+    </header>
 
-<main>
-</main>
+    <main>
+    </main>
 
-<footer>
-<p>© 2023 Delivery Service</p>
-</footer>
-
-<script src="app.js"></script>
-</body>
-</html>
-
+    <footer>
+    <script src = "app.js"></script>
+    </body>
+    </html>
 }
