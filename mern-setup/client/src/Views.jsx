b1fc05a0bc1d8b77809fc40919130099ec94e64c
scott_03ou61r <scottzhu61@gmail.com>
2023-11-13 02:04:07 -0500

    New Views

--- mern-setup/client/src/Views.jsx
+++ mern-setup/client/src/Views.jsx
@@ -1,13 +1,26 @@
 class Views {
-    void serviceSearch() {
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        <title>Delivery Service</title>
+</head>
+<body>
+<header>
+<h1>Delivery Service</h1>
+<input type="text" id="searchInput" placeholder="Search for services">
+</header>
 
-    }
+<main>
+</main>
 
-    void idTracking() {
+<footer>
+<p>© 2023 Delivery Service</p>
+</footer>
 
-    }
+<script src="app.js"></script>
+</body>
+</html>
 
-    void delinatePrices() {
-
-    }
 }
