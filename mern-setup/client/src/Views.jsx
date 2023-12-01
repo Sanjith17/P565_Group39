ab64950f33b2288fb6e0fa349edf402ba98ae5c0
Szhu037 <70656968+Szhu037@users.noreply.github.com>
2023-12-01 04:17:30 -0500

    Views backend

--- mern-setup/client/src/Views.jsx
+++ mern-setup/client/src/Views.jsx
@@ -3,13 +3,13 @@ export default function Views() {
     <head>
         <meta charSet = "UTF-8"
         <meta name = "viewport" content = "width=device-width, intial-scale = 1.0">
-            <link rel = "stylesheet" href = "style.css">
+            <link> rel = "stylesheet" href = "style.css"</link>
                 <title>Delivery Service</title>
             </head>
     <body>
     <header>
         <h1>Delivery Service</h1>
-        <input type = "text" id = "inputSearch" placeholder = "Searching">
+        <input> type = "text" id = "inputSearch" placeholder = "Searching"</input>
         </header>
     <main>
         <section id = "Public_View">
@@ -29,7 +29,7 @@ export default function Views() {
                         <br>
                             <label htmlFor="review">Submit Review:</label>
                                 <textarea id="review" name="review"></textarea>
-                            <br>
+                            </br>
                                 <button type="submit">Submit Order</button>
                 </form>
             </section>
@@ -46,10 +46,42 @@ export default function Views() {
         <footer>
             <script src = "app.js"></script>
             <script>
+                const pkAddress = ["Address 1", "Address 2", "Address 3"]
+
+                const pickupAddressList = document.getElementById("Pickup_address_list")
+                pickupAddresses.forEach(address => {
+                const listItem.textContent = address;
+                pickupAddressList.appendChild(listItem);
+            });
+                document.getElementById("delegatee_delivery").addEventListener("click", function() {
+                document.getElementById("pickupAddresses").style.display = "block";
+            });
+                const destinationAddresses = ["Destination 1", "Destination 2", "Destination 3"];
+                const destinationAddressList = document.getElementById("destination_address_list");
+                destinationAddresses.forEach(address => {
+                    const listItem = document.createElement("li");
+                    listItem.textContent = address;
+                    destinationAddressList.appendChild(listItem);});
+
+                document.getElementById("orderTracker").addEventListener("click", function () {
+                document.getElementById("destination_addresses").style.display = "block";
+            });
+                alert("Delivery completed. Customer notified through email.");
             </script>
         </footer>
         </body>
-        </html>
+
+        <section id = "pickup_addresses" style = "display: none;">
+            <h2>Pickup Addresses</h2>
+            <ul> id = "pickup addresses list"</ul>
+        </section>
+        <section id = "destination_addresses" style= "display: none;">
+            <h2>Destination Addresses</h2>
+            <ul>id = "destination_address_list"</ul>
+        </section>
+
+
+    </html>
 }
 
 
