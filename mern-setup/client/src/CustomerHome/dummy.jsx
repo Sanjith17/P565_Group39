1ef5b31a147ada0577bf9ca4a27340c109672160
Sanjith17 <65133265+Sanjith17@users.noreply.github.com>
2023-12-04 01:26:14 -0500

    small changes on assign driver

--- mern-setup/client/src/CustomerHome/dummy.jsx
+++ mern-setup/client/src/CustomerHome/dummy.jsx
@@ -1,110 +1,192 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import './DriverPage.css';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { Link, useNavigate, Outlet, useLocation} from 'react-router-dom';
+import axios from "axios";  
 
-function RentedBooksPage({ userId }) {
-  const [rentedBooks, setRentedBooks] = useState({ current: [], past: [] });
+const DriverPage = () => {
+  const [addresses, setAdresses] = useState();
+  const navigate = useNavigate(); 
+  const [state, setState] = useState({
+    currentLocation: { lat: 40.712776, lng: -74.005974 },
+    pickUpAddress: '123 PickUp St, City',
+    dropAddress: '456 DropOff Ave, City',
+    pastLocations: ['123 PickUp St, City'],
+    currentLocationName: 'Current Location',
+  });
 
   useEffect(() => {
-
-          const token = localStorage.getItem('token');
-          if (!token) {
-            alert('No token found. Please log in.');
-            return;
-          }
-
-         const fetchBooks = async () => {
-            try {
-              const response = await axios.get('http://localhost:8080/rentedbooks/getbooks',{
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                },
-              });
-              setRentedBooks(response.data)
-            } catch (error) {
-              console.error('Error fetching rented books:', error);
-            }
+    const fetchData = async () => {
+        // Retrieve the JWT token from local storage
+        const token = localStorage.getItem('loginToken');
+  
+        // Check if the token exists
+        if (token) {
+          console.log("got the token");
+          // Set up the headers for the API request with the JWT token
+          const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
           };
-      
-          fetchBooks();
-        
-  }, []);
-
-  const handleReturnBook = async (rentedBookId) => {
-    try {
-      const response = await axios.put(`http://localhost:8080/rentedbooks/update-status/${rentedBookId}`, 
-        { status: 'returned' }, 
-        { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
-      );
-      console.log(response.data)
-      if (response.status === 200) {
-        // Update the state to reflect the change
-        setRentedBooks(prevState => ({
-          ...prevState,
-          current: prevState.current.filter(book => book._id !== rentedBookId),
-          past: [...prevState.past, response.data] // Add the returned book to past rentals
-        }));
-      }
-    } catch (error) {
-      console.error('Error returning book:', error);
+  
+          try {
+            // Make the API request with the token in the headers
+            const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/getuser', {
+              method: 'POST', // or 'POST', 'PUT', etc.
+              headers: headers,
+            });
+            const responseJSON = await response.json();
+            const role = responseJSON.userDetails.role
+          if (role != 'driver'){
+              navigate('/login')
     }
+  
+            
+          } catch (error) {
+            // Handle any errors that occur during te API request
+            console.error(error);
+          }
+        } else {
+          // Handle the case where the token is not found in local storage
+          console.error('JWT token not found in local storage');
+        }
+      };
+  
+      fetchData();
+
+      const getOrders = async() => {
+        const token = localStorage.getItem('loginToken');
+    
+        try {
+          const response = await axios.post(
+            process.env.REACT_APP_BACKEND_URL + "/getdriveraddress",{jwt:token}
+          );
+          const responseJSON = response.data;
+          const orderList = responseJSON.orders;
+          console.log(orderList)
+          setAdresses(orderList)
+  
+        } catch (error) {
+          console.error("Error fetching services data:", error.message);
+        }
+    
+        
+      };
+  
+      getOrders();
+    }, []); 
+
+    useEffect(() => {
+      // Add any additional logic that depends on the updated state
+    }, [addresses]);
+
+  const onMapClick = async (e) => {
+    const newLocation = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    const locationName = await getPlaceName(newLocation);
+    setState({ ...state, currentLocation: newLocation, currentLocationName: locationName });
   };
 
-  const handleReviewSubmit = async (e, ownerId) => {
-    e.preventDefault();
-    const { rating, review } = e.target.elements;
-    
+  const getPlaceName = async (location) => {
+    return `Mock Place Name for (${location.lat.toFixed(3)}, ${location.lng.toFixed(3)})`;
+  };
+
+  const handleLocationUpdate = async() => {
+    setState((prevState) => ({
+      ...prevState,
+      pastLocations: [...prevState.pastLocations, prevState.currentLocationName],
+    }));
+    const token = localStorage.getItem('loginToken');
+
     try {
-      console.log(ownerId)
-      const response = await axios.post(`http://localhost:8080/rentedbooks/review/${ownerId}`, { 
-        rating: rating.value,
-        comment: review.value
-      }, {
-        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-      });
-      console.log(response.data);
-      // Update UI to reflect the new review
+      const response = await axios.post(
+        process.env.REACT_APP_BACKEND_URL + "/setdriverlocation",{location: state.currentLocation,jwt:token}
+      );
     } catch (error) {
-      console.error('Error submitting review:', error);
+      console.error("Error fetching services data:", error.message);
     }
-  };  
+
+    console.log(state.currentLocation);
+  };
+
+  const mapStyles = { height: '300px', width: '100%' };
+
+  const handlePickUp = () => {
+    // Perform pick-up logic
+    onPickUp(addresses[pickupIndex].destinationaddress);
+
+    // Move to the next pickup address
+    setPickupIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handleDeliver = (index) => {
+    // Perform delivery logic
+    onDeliver(index);
+  };
+
+
 
   return (
-    <div className="rented-books">
-      <h1>My Rented Books</h1>
-      <h2>Current Rentals</h2>
-      <ul>
-        {rentedBooks.current.map(book => (
-          <li key={book._id}>
-            <img src={book.image} alt={book.title} />
-            <h3>{book.title}</h3>
-            <p>Author: {book.author}</p>
-            <p>Rented for {book.days} days</p>
-            <p>Price per day: ${book.price_per_day}</p>
-            <button onClick={() => handleReturnBook(book._id)}>Return</button>
-            {/* Add more details as needed */}
-          </li>
+    <div className="driver-page">
+
+<div>
+        <h2>Pickup Addresses</h2>
+        {addresses.map((address, index) => (
+          <div key={index}>
+            {index === pickupIndex && (
+              <>
+                <p>{address.destinationaddress}</p>
+                <button onClick={handlePickUp} disabled={pickupIndex !== index}>
+                  Pick Up
+                </button>
+              </>
+            )}
+          </div>
         ))}
-      </ul>
-      <h2>Past Rentals</h2>
-      <ul>
-        {rentedBooks.past.map(book => (
-          <li key={book._id}>
-            <img src={book.image} alt={book.title} />
-            <h3>{book.title}</h3>
-            <p>Author: {book.author}</p>
-            <p>Rented for {book.days} days</p>
-            <p>Price per day: ${book.price_per_day}</p>
-            <form onSubmit={(e) => handleReviewSubmit(e, book.owner_user_id)}>
-      <input type="number" name="rating" min="1" max="5" required placeholder="Rating (1-5)" />
-      <textarea name="review" required placeholder="Write a review about the owner"></textarea>
-      <button type="submit">Submit Review</button>
-    </form>
-            {/* Add more details as needed */}
-          </li>
+      </div>
+
+      <div>
+        <h2>Delivery Addresses</h2>
+        {addresses.map((address, index) => (
+          <div key={index}>
+            {index < pickupIndex && (
+              <>
+                <p>{address.sourceaddress}</p>
+                <button onClick={() => handleDeliver(index)}>Deliver</button>
+              </>
+            )}
+          </div>
         ))}
-      </ul>
+      </div>
+
+      
+      
+
+      <LoadScript googleMapsApiKey="AIzaSyBmMGeMKJDnzvKkm8k1RADdiP2N632RfNs">
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          center={state.currentLocation}
+          onClick={onMapClick}
+        >
+          <Marker position={state.currentLocation} />
+        </GoogleMap>
+      </LoadScript>
+
+      <div className="location-update-box">
+        <p>Update Location: {state.currentLocationName}</p>
+        <button onClick={handleLocationUpdate}>Update Location</button>
+      </div>
+
+      <div className="history-box">
+        <h3>Past Locations:</h3>
+        <ul>
+          {state.pastLocations.map((location, index) => (
+            <li key={index}>{location}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
-export default RentedBooksPage;
+export default DriverPage;
--- mern-setup/server/routes/user.js
+++ mern-setup/server/routes/user.js
@@ -96,6 +96,7 @@ router.post("/setdrivers", getusercont.set_drivers);
 router.post("/setreview", getusercont.set_review);
 router.post("/setdriverlocation", getusercont.set_driver_location);
 router.post("/trackorder", getusercont.trackorder);
+router.post("/getdriveraddress", getusercont.get_driver_add);
 
 
 router.post("/admin_form2", form2.company_services);
