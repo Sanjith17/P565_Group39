2b1d95218fab326cb7cd1cbcb0915d86dd29186d
Szhu037 <70656968+Szhu037@users.noreply.github.com>
2023-12-04 20:54:38 -0500

    Email notifications, public view, and public view ratings

--- mern-setup/client/src/Rating/Rate.jsx
+++ mern-setup/client/src/Rating/Rate.jsx
@@ -2,4 +2,34 @@ import React, { useState } from "react";
 import "./RatingAndReview.css";
 import { Link } from "react-router-dom";
 
+const RatingAndReview = () => {
+    const [reviews, setReviews] = useState([
+        {
+            id: 1,
+            header: "excellent product",
+            content: "this product was really good",
+            rating: 10,
+            productName: "Shoes",
+        }
+    ]);
+
+    return (
+        <div>
+            {reviews.map(review => (
+                <div key = {review.id} className = "review-container">
+                    <div className = "review-header">{review.header}</div>
+                    <div className = "review">
+                        {review.content}
+                    </div>
+                    <div className="rating">Rating: {review.rating}</div>
+                    <div className="proName">Product: {review.productName}</div>
+                    <div className="proReview"></div>
+                </div>
+            ))}
+                    <Link to = "/">Back to home</Link>
+        </div>
+    );
+};
+
+export default RatingAndReview;
 
